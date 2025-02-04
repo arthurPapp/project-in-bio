0df3ccca6780cf907213fd4aedbbe3ca80f8f601
P0643617 <59942836+arthurPapp@users.noreply.github.com>
2025-02-04 09:28:36 -0300

    commit criar

---
+++ app/(pages)/criar/page.tsx
@@ -0,0 +1,29 @@
+import { Rocket } from "lucide-react";
+import Header from "../../components/landing-page/header";
+import TextInput from "../../components/ui/text-input";
+import Button from "../../components/ui/button";
+
+export default function CriarPage() {
+  return (
+    <div>
+      <Header />
+      <div className="h-screen flex flex-col gap-10 items-center justify-center max-w-xl mx-auto">
+        <div className="flex items-center gap-4">
+          <h1 className="text-4xl font-bold text-white">Escolha seu link</h1>
+          <Rocket className="size-10" />
+        </div> 
+        <form
+          action=""
+          className="w-full flex items-center gap-2"
+        >
+          <span className="text-white">projectinbio.com/</span>
+          <TextInput />
+          <Button className="w-[126px]">Criar</Button>
+        </form>
+        <div>
+          <span className="text-accent-pink">Error de Exmplo</span>
+        </div>
+      </div>
+    </div>
+  )
+}
