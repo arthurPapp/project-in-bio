8c84f25fb2a25f2cc1ef9d8fc23dce928575320d
P0643617 <59942836+arthurPapp@users.noreply.github.com>
2025-03-16 19:23:48 -0300

    auth + googleConfig

--- app/components/landing-page/header.tsx
+++ app/components/landing-page/header.tsx
@@ -1,6 +1,10 @@
+import { manageAuth } from "../../actions/manage-auth";
+import { auth } from "../../lib/auth";
 import Button from "../ui/button";
 
-export default function Header() {
+ 
+ export default async function Header() {
+   const session = await auth();
   return (
     <div className="absolute left-0 right-0 max-w-7xl mx-auto flex justify-between items-center py-10">
       <div className="flex items-center gap-4">
@@ -8,8 +12,10 @@ export default function Header() {
         <h3 className="text-white text-2xl font-bold">ProjectInBio</h3>
       </div>
       <div className="flex items-center gap-4">
-        <Button>Minha Pagina</Button>
-        <Button>Sair</Button>
+      {session && <Button>Minha Página</Button>}
+         <form action={manageAuth}>
+           <Button>{session ? "Sair" : "Login"}</Button>
+         </form>
       </div>
     </div>
   );
