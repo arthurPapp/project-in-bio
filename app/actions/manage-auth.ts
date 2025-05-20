f39100eea07b067023c56a27a07d3e70e8312de3
P0643617 <59942836+arthurPapp@users.noreply.github.com>
2025-05-20 11:27:52 -0300

    add logs

--- app/actions/manage-auth.ts
+++ app/actions/manage-auth.ts
@@ -5,9 +5,11 @@
  export async function manageAuth() {
    const session = await auth();
  
+   //redireciona para apagina de loggin do google, depois de logar ele vai para pagina criar
    if (!session) {
      return await signIn("google", { redirectTo: "/criar" });
    }
  
+   //retorna para pagina inicial
    return await signOut({ redirectTo: "/" });
  }
